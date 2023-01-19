2ec97d105346a9954d1358c04fee7675b8e5f2b2
Seth Minor <97004318+SethMinor@users.noreply.github.com>
2023-01-19 15:29:37 -0800

    Update poincare_sections.m

--- poincare_sections.m
+++ poincare_sections.m
@@ -72,98 +72,87 @@ lambda =@(phi,theta) gamma(phi,theta)./c;
 cap = 20;     % truncation error
 p = exp(-gr); % nome (for periodicity)
 
-%% Initial conditions for vortices
-% Initial vortex positions in [-pi*c,pi*c]x[cgl,cgr]
-% [-pi*c,pi*c] = [-33.2475, 33.2475]
-% [cgl,cgr] = [-10.5830, 10.5830]
-w1_0 = (0) + 1i*(2.0); % positive vortex
-w2_0 = (1) + 1i*(-2.0); % negative vortex
 
-% Vortex charges
-q1 = 1;
-q2 = -1;
-q = [q1 q2];   % vector of vortex charges
-N = length(q); % keeping track of number of vortices
-
-% real and imaginary parts of isothermal coords
-u1_0 = real(w1_0);
-v1_0 = imag(w1_0);
-
-u2_0 = real(w2_0);
-v2_0 = imag(w2_0);
-
-%% Compute Poincare sections
-% ode45 with events function
-t0 = 0;
-tf = 2500;
-timespan = [t0, tf];
-options = odeset('RelTol', 1e-12, 'AbsTol', 1e-12, 'Events', @EventsFcn);
-
-% Numerical integration using ode45 or ode15s
-y0 = [u1_0, u2_0, v1_0, v2_0];
-[T,Y,Te,Ye,Ie] = ode15s(@(t,y) vortex_velocity_v2(0,y,0,N,q,r,a,R,c,p,cap,theta,Dginv,gr),...
-    timespan, y0, options);
-
-%% Change coordinates from numerical solution
-% Vortices in isothermal coordinates
-U = Y(:,1:N); % u-coords
-U = UVwrap(U, [-pi*c, pi*c]);
-
-V = Y(:,(1+N):2*N); % v-coords
-V = UVwrap(V, [c*gl, c*gr]);
-
-% Combined complex coordinate
-W = U + 1i*V;
-
-% Toroidal-poloidal coordinates
-Phi = U./c;
-Theta = [theta(V(:,1)), theta(V(:,2))];
-
-%% Plot integrated solution
-% Some nice RGB colores
-bluey = [0 0.4470 0.7410];
-orangu = [0.8500 0.3250 0.0980];
-
-figure (2)
-
-% Isothermal orbit
-subplot(2,1,1)
-plot(U,V,'.','MarkerSize',2)
-grid on
-xlabel('$u = $Re$(w)$','Interpreter','latex','FontSize',fs)
-ylabel('$v = $Im$(w)$','Interpreter','latex','FontSize',fs)
-title('Isothermal Coordinates','Interpreter','latex','FontSize',fs)
-xlim([-pi*c, pi*c])
-ylim([c*gl, c*gr])
-
-% Toriodal-poloidal orbit
-subplot(2,1,2)
-plotwrapped(Phi(:,1),Theta(:,1),1, [-pi, pi],[-pi, pi], 0.05, bluey)
-hold on
-plotwrapped(Phi(:,2),Theta(:,2),1, [-pi, pi],[-pi, pi], 0.05, orangu)
-hold off
-xlabel('$\phi$','Interpreter','latex','FontSize',fs)
-ylabel('$\theta$','Interpreter','latex','FontSize',fs)
-title('Toroidal-Poloidal Coordinates','Interpreter','latex','FontSize',fs)
-xlim([-pi, pi])
-ylim([-pi, pi])
-
-%% Plot Poincare section
-% Coordinates at Poincare crossings
-Ue = Ye(:,1:N); % u-coords
-Ue = UVwrap(Ue, [-pi*c, pi*c]);
-
-Ve = Ye(:,(1+N):2*N); % v-coords
-Ve = UVwrap(Ve, [c*gl, c*gr]);
-
-% Conversion to toroidal-poloidal coordinates
-Phi_e = Ue./c;
-Theta_e = [theta(Ve(:,1)), theta(Ve(:,2))];
-
-% Poincare section
-figure (3)
-plot(Phi_e(:,1), Phi_e(:,2),'.')
-hold on
+%% Giant for-loop over ICs
+for u1pert = -1:0.1:1
+    for u2pert = -2:0.1:2
+        for v1pert = -5:0.1:5
+            for v2pert = -1:0.1:1
+                % Initial conditions for vortices
+                % Initial vortex positions in [-pi*c,pi*c]x[cgl,cgr]
+                % [-pi*c,pi*c] = [-33.2475, 33.2475]
+                % [cgl,cgr] = [-10.5830, 10.5830]
+                w1_0 = (u1pert) + 1i*(v1pert); % positive vortex
+                w2_0 = (u2pert) + 1i*(v2pert); % negative vortex
+                
+                % Vortex charges
+                q1 = 1;
+                q2 = -1;
+                q = [q1 q2];   % vector of vortex charges
+                N = length(q); % keeping track of number of vortices
+                
+                % real and imaginary parts of isothermal coords
+                u1_0 = real(w1_0);
+                v1_0 = imag(w1_0);
+                
+                u2_0 = real(w2_0);
+                v2_0 = imag(w2_0);
+            
+                % Compute Poincare sections
+                % ode45 with events function
+                t0 = 0;
+                tf = 1500;
+                timespan = [t0, tf];
+                options = odeset('RelTol', 1e-11, 'AbsTol', 1e-11, 'Events', @EventsFcn);
+                
+                % Numerical integration using ode45 or ode15s
+                y0 = [u1_0, u2_0, v1_0, v2_0];
+                [T,Y,Te,Ye,Ie] = ode15s(@(t,y) vortex_velocity_v2(0,y,0,N,q,r,a,R,c,p,cap,theta,Dginv,gr),...
+                    timespan, y0, options);
+                
+                % Change coordinates from numerical solution
+                % Vortices in isothermal coordinates
+                U = Y(:,1:N); % u-coords
+                U = UVwrap(U, [-pi*c, pi*c]);
+                
+                V = Y(:,(1+N):2*N); % v-coords
+                V = UVwrap(V, [c*gl, c*gr]);
+                
+                % Plot integrated solution
+                % Some nice RGB colores
+                bluey = [0 0.4470 0.7410];
+                orangu = [0.8500 0.3250 0.0980];
+                
+                % Isothermal orbit
+                figure (2)
+                plot(U,V,'.','MarkerSize',2)
+                grid on
+                xlabel('$u = $Re$(w)$','Interpreter','latex','FontSize',fs)
+                ylabel('$v = $Im$(w)$','Interpreter','latex','FontSize',fs)
+                title('Isothermal Coordinates','Interpreter','latex','FontSize',fs)
+                xlim([-pi*c, pi*c])
+                ylim([c*gl, c*gr])
+                
+                % Plot Poincare section
+                % Coordinates at Poincare crossings
+                Ue = Ye(:,1:N); % u-coords
+                Ue = UVwrap(Ue, [-pi*c, pi*c]);
+                
+                Ve = Ye(:,(1+N):2*N); % v-coords
+                Ve = UVwrap(Ve, [c*gl, c*gr]);
+                
+                % Conversion to toroidal-poloidal coordinates
+                Phi_e = Ue./c;
+                Theta_e = [theta(Ve(:,1)), theta(Ve(:,2))];
+                
+                % Poincare section
+                figure (3)
+                plot(Phi_e(:,1), Phi_e(:,2),'.')
+                hold on
+            end
+        end
+    end
+end
 
 %% Function definitions
 % RHS of nonlinear isothermal coords ODE
